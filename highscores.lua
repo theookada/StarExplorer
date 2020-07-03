31a5723fed38b7e0ce9e9103d443ede292041c70
Theo Okada <theodoreokada@gmail.com>
2020-07-03 10:56:23 +0900

    highscores

--- highscores.lua
+++ highscores.lua
@@ -8,12 +8,47 @@ local scene = composer.newScene()
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
 
-local function gotoGame()
-	composer.gotoScene( "game", { time=800, effect="crossFade" } )
+-- Initialiaze variables
+local json = require( "json" )
+
+local scoresTable = {}
+
+local filePath = system.pathForFile( "scores.json", system.DocumentsDirectory )
+
+
+local function loadScores ()
+
+	local file = io.open( filePath, "r" )
+
+	if file then
+		local contents = file:read( "*a" )
+		io.close( file )
+		scoresTable = json.decode( contents )
+	end
+
+	if ( scoresTable == nil or #scoresTable == 0 ) then scoresTable = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+
+	end
 end
 
-local function gotoHighScores()
-	composer.gotoScene( "highscores", { time=800, effect="crossFade" } )
+
+local function saveScores()
+
+	for i = #scoresTable, 11, -1 do
+		table.remove( scoresTable, i )
+	end
+
+	local file = io.open( filePath, "w" )
+
+	if file then
+		file:write( json.encode( scoresTable ) )
+		io.close( file )
+	end
+end
+
+
+local function gotoMenu()
+	composer.gotoScene( "menu", { time=800, effect="crossFade" } )
 end
 
 
@@ -27,24 +62,45 @@ function scene:create( event )
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 
+	-- Load the previous scores
+	loadScores()
+
+	-- Insert the saved score from the last game into the table, then reset it
+	table.insert( scoresTable, composer.getVariable( "finalScore" ) )
+	composer.setVariable( "finalScore", 0 )
+
+	-- Sort the table entries from highest to lowest
+	local function compare( a, b )
+		return a > b
+	end
+	table.sort( scoresTable, compare )
+
+	-- Save the scores
+	saveScores()
+
 	local background = display.newImageRect( sceneGroup, "background.png", 800, 1400 )
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
 
-	local title = display.newImageRect( sceneGroup, "title.png", 500, 80 )
-	title.x = display.contentCenterX
-	title.y = 200
+	local highScoresHeader = display.newText( sceneGroup, "High Scores", display.contentCenterX, 100, native.systemFont, 44 )
 
-	local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 700, native.systemFont, 44 )
-	playButton:setFillColor( 0.82, 0.86, 1 )
+	for i = 1, 10 do
+		if ( scoresTable[i] ) then
+			local yPos = 150 + ( i * 56 )
 
-	local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX, 810, native.systemFont, 44 )
-	highScoresButton:setFillColor( 0.75, 0.78, 1 )
+			local rankNum = display.newText( sceneGroup, i .. ")", display.contentCenterX-50, yPos, native.systemFont, 36 )
+			rankNum:setFillColor( 0.8 )
+			rankNum.anchorX = 1
 
-	playButton:addEventListener( "tap", gotoGame )
-	highScoresButton:addEventListener( "tap", gotoHighScores)
+			local thisScore = display.newText( sceneGroup, scoresTable[i], display.contentCenterX-30, yPos, native.systemFont, 36 )
+			thisScore.anchorX = 0
+		end
+	end
+
+	local menuButton = display.newText( sceneGroup, "Menu", display.contentCenterX, 810, native.systemFont, 44 )
+	menuButton:setFillColor( 0.75, 0.78, 1 )
+	menuButton:addEventListener( "tap", gotoMenu )
 end
-
 
 -- show()
 function scene:show( event )
@@ -73,7 +129,7 @@ function scene:hide( event )
 
 	elseif ( phase == "did" ) then
 		-- Code here runs immediately after the scene goes entirely off screen
-
+		composer.removeScene( "highscores" )
 	end
 end
 
